bc31625dee15f9960e98623f7d0ab36786087cb0
SiebeCluyts <debendevanzelem1@gmail.com>
2026-04-23 17:30:04 +0200

    switched dark comments

--- article.js
+++ article.js
@@ -70,7 +70,7 @@
   script.setAttribute('data-reactions-enabled', '1');
   script.setAttribute('data-emit-metadata', '0');
   script.setAttribute('data-input-position', 'top');
-  script.setAttribute('data-theme', 'preferred_color_scheme');
+  script.setAttribute('data-theme', 'dark');
   script.setAttribute('data-lang', 'en');
   giscusContainer.appendChild(script);
     if (cookiesAccepted()) {
